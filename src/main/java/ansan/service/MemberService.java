da70296779f204a51083266c9a3a65b03dff0146
itdanja <itdanja@kakao.com>
2022-01-05 17:01:02 +0900

    Day04 메일보내기

--- src/main/java/ansan/service/MemberService.java
+++ src/main/java/ansan/service/MemberService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import javax.transaction.Transactional;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Random;
@@ -60,6 +61,7 @@ public class MemberService {
     private JavaMailSender javaMailSender;  //  자바메일 객체
 
     // 회원 비밀번호 찾기 -> 메일 전송 [ 임시비밀번호 ]
+    @Transactional
     public boolean findpassword( MemberDto memberDto ) {
         List<MemberEntity> memberEntities = memberRepository.findAll();
         for( MemberEntity memberEntity  :  memberEntities){
@@ -71,20 +73,23 @@ public class MemberService {
                 StringBuilder body = new StringBuilder();   // StringBuilder  : 문자열 연결 클래스  [ 문자열1+문자열2 ]
                 body.append("<html> <body><h1> Ansan 계정 임시 비밀번호 </h1>");    // 보내는 메시지에 html 추가
 
-                Random random = new Random();
-                // 임시 비밀번호 만들기
-                StringBuilder temppassword = new StringBuilder();
-                for( int i = 0 ; i<12 ; i++ ){  // 12 자리 만들기
-                    // 랜덤 숫자 -> 문자 변환
-                    temppassword.append( (char)((int)(random.nextInt(26))+97) );
-                }
-                body.append("<div>"+temppassword+"</div></html>");      // 보내는 메시지에 임시비밀번호를 html에 추가
+                    Random random = new Random();
+                    // 임시 비밀번호 만들기
+                    StringBuilder temppassword = new StringBuilder();
+                    for( int i = 0 ; i<12 ; i++ ){  // 12 자리 만들기
+                        // 랜덤 숫자 -> 문자 변환
+                        temppassword.append( (char)((int)(random.nextInt(26))+97) );
+                    }
+                    body.append("<div>"+temppassword+"</div></html>");      // 보내는 메시지에 임시비밀번호를 html에 추가
 
-                // Mime : 전자우편 포멧 프로토콜[통신 규약]
-                // SMTP : 전자우편 전송 프로토콜 [ 통신 규약 ]
-                try {       //
-                    MimeMessage message = javaMailSender.createMimeMessage();
-                    MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true, "utf-8");
+                    // 엔티티내 패스워드 변경
+                    memberEntity.setM_password( temppassword.toString() );     //JPA
+
+                    // Mime : 전자우편 포멧 프로토콜[통신 규약]
+                    // SMTP : 전자우편 전송 프로토콜 [ 통신 규약 ]
+                    try {       //
+                        MimeMessage message = javaMailSender.createMimeMessage();
+                        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true, "utf-8");
 
                     mimeMessageHelper.setFrom("kgs2072@naver.com", "Ansan");      // 보내는사람  //  이름
                     mimeMessageHelper.setTo( "kgs2072@naver.com");                                                 //  받는사람
